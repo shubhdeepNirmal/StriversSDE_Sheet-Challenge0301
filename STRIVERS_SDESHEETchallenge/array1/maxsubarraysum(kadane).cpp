4d9242513b455da8ba0fa1465d1e3172406e3ca0
Shubhdeep Nirmal <shubhdeepnirmal@gmail.com>
2023-06-07 02:46:36 +0530

    Create maxsubarraysum(kadane).cpp

---
+++ STRIVERS_SDESHEETchallenge/array1/maxsubarraysum(kadane).cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+long long maxSubarraySum(int arr[], int n)
+{
+    long long sum=0;
+    long long maxi= INT_MIN;
+  
+  for(int i=0;i<n;i++)
+  {
+      sum+=arr[i];
+      if(sum<0)
+      {
+        sum=0;
+      } 
+     maxi=max(sum,maxi);
+  }
+    return maxi;
+}
