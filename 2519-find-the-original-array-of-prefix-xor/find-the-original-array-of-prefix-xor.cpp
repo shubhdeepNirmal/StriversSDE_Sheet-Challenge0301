9a47d2cc9eb984623f1e2c314adf7186b7233c7f
Shubhdeep Nirmal <shubhdeepnirmal@gmail.com>
2023-10-31 20:21:04 +0530

    Time: 92 ms (32.48%) | Memory: 79.8 MB (7.61%) - LeetSync

---
+++ 2519-find-the-original-array-of-prefix-xor/find-the-original-array-of-prefix-xor.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) {
+
+        int n= pref.size();
+        vector<int>nums;
+         nums.push_back(pref[0]);
+        for(int i=1;i<n;i++)
+        {
+         nums.push_back(pref[i]^pref[i-1]);
+        }
+
+        return nums;
+    }
+};
