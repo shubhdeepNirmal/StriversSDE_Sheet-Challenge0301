ca863e3b44629c3bfea5fc7e64984cb237b74de8
Shubhdeep Nirmal <shubhdeepnirmal@gmail.com>
2023-11-05 11:09:25 +0530

    Time: 93 ms (37.04%) | Memory: 63.6 MB (29.63%) - LeetSync

---
+++ 1657-find-the-winner-of-an-array-game/find-the-winner-of-an-array-game.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int getWinner(vector<int>& arr, int k) {
+        int n=arr.size();
+            int maxi=INT_MIN;
+            for(int i=0;i<n;i++)
+            {
+               maxi=max(maxi,arr[i]);
+            }
+
+            if(k>=n) return maxi;
+            int curr=arr[0];
+            int streak=0;
+
+            for(int i=1;i<n;i++)
+            {
+                int opp=arr[i];
+                if(curr>opp)   streak++;
+                else{
+                    curr=opp;
+                    streak=1;
+                }
+                if(streak==k||curr==maxi) return curr;
+            }  
+    
+      return -1;
+        
+    }
+};
