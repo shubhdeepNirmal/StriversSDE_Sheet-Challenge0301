ed30a8236293d5464d1195d1dbafeb4f4c4d67de
Shubhdeep Nirmal <shubhdeepnirmal@gmail.com>
2024-02-22 16:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (40.53%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,35 +1,37 @@
 class Solution {
 public:
-    int countpartitions(vector<int> & nums, int maxsum){
-        int n= nums.size();
+    bool isvalid(vector<int> & nums, int n, int k , int mx){
         int partitions=1;
         long long subsum=0;
         for(int i=0;i<n;i++)
         {
-            if(subsum+nums[i]<=maxsum){
-                subsum+=nums[i];
+            subsum+=nums[i];
+            if(subsum>mx){
+                 partitions++;
+                 subsum=nums[i];
             }
-            else{
-                partitions++;
-                subsum=nums[i];
+            if(partitions>k){
+                return false;
             }
         }
-        return partitions;
+        return true;
     }
     int splitArray(vector<int>& nums, int k) {
+        int n= nums.size();
+        int sum=0;
         int low= *max_element(nums.begin(),nums.end());
         int high= accumulate(nums.begin(),nums.end(),0);
+        int ans=-1;
         while(low<=high){
             int mid = low+(high-low)/2;
-            int part= countpartitions(nums,mid);
-
-            if(part>k){
-                low= mid+1;
+            if(isvalid(nums,n,k,mid)){
+                 ans=mid;
+                 high=mid-1;
             }
             else{
-                high=mid-1;
+                low= mid+1;
             }
         }
-     return low; 
+     return ans; 
     }
 };
