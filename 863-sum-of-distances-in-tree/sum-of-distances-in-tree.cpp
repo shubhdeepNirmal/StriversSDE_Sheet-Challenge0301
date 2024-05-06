0f8f2e3027d74cf8f1a3842416d239a07ddb886f
Shubhdeep Nirmal <shubhdeepnirmal@gmail.com>
2024-05-06 14:56:29 +0530

    Time: 181 ms (88.53%) | Memory: 96.5 MB (61.14%) - LeetSync

---
+++ 863-sum-of-distances-in-tree/sum-of-distances-in-tree.cpp
@@ -0,0 +1,51 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+    vector<int> ans, count, subTree;
+    vector<vector<int>> graph;
+    int N;
+
+public:
+    vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
+        N = n;
+        graph.resize(N);
+        ans.resize(N);
+        count.resize(N);
+        subTree.resize(N);
+
+        for (int i = 0; i < N; ++i)
+            graph[i] = vector<int>();
+        
+        for (auto& edge : edges) {
+            int u = edge[0], v = edge[1];
+            graph[u].push_back(v);
+            graph[v].push_back(u);
+        }
+        
+        dfs(0, -1);
+        ans[0] = subTree[0];
+        dfs2(0, -1);
+        return ans;
+    }
+
+    void dfs(int node, int parent) {
+        for (int child : graph[node]) {
+            if (child != parent) {
+                dfs(child, node);
+                count[node] += count[child];
+                subTree[node] += subTree[child] + count[child];
+            }
+        }
+        count[node] += 1;
+    }
+
+    void dfs2(int node, int parent) {
+        for (int child : graph[node]) {
+            if (child != parent) {
+                ans[child] = ans[node] - count[child] + N - count[child];
+                dfs2(child, node);
+            }
+        }
+    }
+};
