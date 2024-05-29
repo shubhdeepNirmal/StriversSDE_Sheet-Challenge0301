88dd818e5378cd92f2854add1a7938a1d1b6c55c
Shubhdeep Nirmal <shubhdeepnirmal@gmail.com>
2024-05-29 15:25:13 +0530

    Time: 116 ms (46.55%) | Memory: 50.7 MB (73.56%) - LeetSync

--- 1824-maximum-number-of-eaten-apples/maximum-number-of-eaten-apples.cpp
+++ 1824-maximum-number-of-eaten-apples/maximum-number-of-eaten-apples.cpp
@@ -6,31 +6,25 @@ public:
         int ans=0;
         int n=apples.size();
         int i=0;
-
         priority_queue<pii, vector<pii>,greater<pii>>pq;
-
         for(;i<n;++i)
         {
             if(apples[i]){
                 pq.push({i+days[i],apples[i]});
             }
-
             while(!pq.empty()&&pq.top().first<=i){
                 pq.pop();
             }
             if(pq.empty()){
                 continue;
             }
-
             pii now=pq.top();
             pq.pop();
-
             if(now.second>1){
                 pq.push({now.first,now.second-1});
             }
             ++ans;
         }
-
         while(!pq.empty()){
             while(!pq.empty()&&pq.top().first<=i){
                 pq.pop();
@@ -39,16 +33,13 @@ public:
                 break;
             }
             ++i;
-
             pii now=pq.top();
             pq.pop();
-
             if(now.second>1){
                 pq.push({now.first,now.second-1});
             }
             ++ans;
         }
-
         return ans;
     }
 };
