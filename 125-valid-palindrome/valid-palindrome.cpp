8476d4d21792603d084349c1ad85232f5bcaba42
Shubhdeep Nirmal <shubhdeepnirmal@gmail.com>
2024-07-20 04:28:49 +0530

    Time: 5 ms (50.54%) | Memory: 8.9 MB (47.65%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,56 +1,19 @@
 class Solution {
-private:
-bool valid(char ch)
-{
-    if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')||(ch>='0'&&ch<='9'))
-    {
-        return 1;
-    }
-    else
-    return 0;
-}
-
-char tolower(char ch)
-{
-    if((ch>='a'&&ch<='z')||(ch>='0'&&ch<='9'))
-    {
-        return ch;
-    }
-    else{
-    char temp= ch-'A'+'a';
-    return temp;
-    }
-}
-
-bool checkpalindrome(string a)
-{
-    int s=0;
-    int e=a.length()-1;
-    while(s<=e)
-    {
-        if((a[s])!=(a[e]))
-        {
-            return 0;
-        }
-        else{
-            s++;
-            e--;
-        }
-    }
-    return 1;
-}
-
 public:
     bool isPalindrome(string s) {
-     int i=0;
-     string temp="";
-     for(int i=0;i<s.length();i++)
-     {
-         if(valid(s[i]))
-         {
-             temp.push_back(tolower(s[i]));
-         }
-     }
-      return checkpalindrome(temp);  
+        int start=0;
+        int end=s.length()-1;
+        while(start<end){
+            while(start<end && !isalnum(s[start])){
+                start++;
+            }
+            while(start<end && !isalnum(s[end])){
+               end--;
+            }
+            if(tolower(s[start])!=tolower(s[end])) return false;
+            start++;
+            end--;
+        }
+        return true;
     }
 };
